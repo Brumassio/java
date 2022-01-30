5fda15d8b0b062e13834d180b692ec504652e1f7
Brumassio <dibrunspeed@gmail.com>
2022-01-30 17:52:06 -0300

    Classe Médico

---
+++ Exemplo_astah_exportado/Medico.java
@@ -0,0 +1,62 @@
+package Exemplo_astah_exportado;
+import java.sql.Date;
+public class Medico {
+
+	private String crm;
+
+	private String nome;
+
+	private String fone;
+
+	private Date dataContratacao;
+
+	private int deptoId;
+
+	private Departamento departamento;
+
+	private void setCrm(String crm) {
+
+	}
+
+	public String getCrm() {
+		return null;
+	}
+
+	private void setNome(String nome) {
+
+	}
+
+	public String getNome() {
+		return null;
+	}
+
+	private void setFone(String fone) {
+
+	}
+
+	public String getFone() {
+		return null;
+	}
+
+	private void setDataContratacao(Date dataContratacao) {
+
+	}
+
+	public Date getDataContratacao() {
+		return null;
+	}
+
+	private void setDeptoMedico(String deptoMedico) {
+		this.deptoId = departamento.getDeptoId();
+	}
+
+	public String getDeptoMedico() {
+		return null;
+	}
+
+	public String buscarDepartamento() {
+		//codigo que da getId e getNome
+		return null;
+	}
+
+}
