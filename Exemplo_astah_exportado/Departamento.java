6ef5718e8b143b11eb966723861b3576224c3e58
Brumassio <dibrunspeed@gmail.com>
2022-01-30 17:51:40 -0300

    Classe Departamento

---
+++ Exemplo_astah_exportado/Departamento.java
@@ -0,0 +1,24 @@
+package Exemplo_astah_exportado;
+public class Departamento {
+
+	private int deptoId;
+
+	private String deptNome;
+
+	private void setDeptoId(int deptoId) {
+
+	}
+
+	public int getDeptoId() {
+		return 0;
+	}
+
+	private void setDeptNome(String deptNome) {
+
+	}
+
+	public String getDeptNome() {
+		return null;
+	}
+
+}
