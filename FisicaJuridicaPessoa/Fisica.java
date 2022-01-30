a018fa0cfd60afbdde8a0562fd9dab51da0b871b
Brumassio <dibrunspeed@gmail.com>
2022-01-30 17:53:34 -0300

    Classe Fisica que herda a Classe Pessoa

---
+++ FisicaJuridicaPessoa/Fisica.java
@@ -0,0 +1,15 @@
+package FisicaJuridicaPessoa;
+
+import java.sql.Date;;
+
+public class Fisica extends Pessoa {
+
+	private String cpf;
+
+	private float salario;
+
+	private Date dataNascimento;
+
+	public Juridica  Juridica;
+
+}
