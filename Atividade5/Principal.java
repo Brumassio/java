d0ca0dcf0f843735d2c8d0a195cb1507aeefd40c
Brumassio <dibrunspeed@gmail.com>
2022-03-16 16:03:14 -0300

    Classe principal !! / main

---
+++ Atividade5/Principal.java
@@ -0,0 +1,13 @@
+package Atividade5;
+
+import java.io.*;
+import principal.TelaPrincipal;
+
+
+public class Principal {
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        TelaPrincipal main = new TelaPrincipal();
+    }
+    
+}
