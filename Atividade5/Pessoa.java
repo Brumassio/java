2a20fd61387cec115f7bd8bd5be83b76bee0beaf
Brumassio <dibrunspeed@gmail.com>
2022-03-16 16:02:49 -0300

    Superclasse Pessoa !<3

---
+++ Atividade5/Pessoa.java
@@ -0,0 +1,54 @@
+package Atividade5;
+
+
+public class Pessoa {
+    //protected int idPessoa;
+    protected String nome;
+    protected String cpf;
+    protected String endereco;
+    protected String telefone;
+    
+    //////////////////////////////////////////////
+
+    //protected int getIdPessoa() {
+    //  return idPessoa;
+    //}
+
+    //protected void setIdPessoa(int idPessoa) {
+    //    this.idPessoa = idPessoa;
+    //}
+
+    protected String getNome() {
+        return nome;
+    }
+
+    protected void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    protected String getCpf() {
+        return cpf;
+    }
+
+    protected void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    protected String getEndereco() {
+        return endereco;
+    }
+
+    protected void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    protected String getTelefone() {
+        return telefone;
+    }
+
+    protected void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    
+}
