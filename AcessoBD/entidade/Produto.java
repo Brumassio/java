e77fe2331bc2ddb24d31b6af57529ffba25daa8e
Brumassio <dibrunspeed@gmail.com>
2022-04-11 15:21:34 -0300

    Classe produto !

---
+++ AcessoBD/entidade/Produto.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidade;
+
+/**
+ *
+ * @author guest-44bnxg
+ */
+public class Produto {
+
+
+
+
+    private int id;
+    private String nome;
+    private String descricao;
+    private int codBarras;
+    private String categoria;
+    private int quantidade;
+    private double valor;
+    
+    ///////////////////
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCodBarras() {
+        return codBarras;
+    }
+
+    public void setCodBarras(int codBarras) {
+        this.codBarras = codBarras;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+    
+    public void setValor(double valor) {
+        this.valor = valor;
+    }    
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    
+}
